85dc5e4de2cc704fc0a1a025ad5325de6e22f367
Ben Botsford <ben.botsford@flatironschool.com>
2022-09-16 11:12:59 -0500

    directory structure, pipfile

--- lib/debug.py
+++ lib/debug.py
@@ -3,7 +3,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from lib.sqlalchemy_sandbox import Student
+from sqlalchemy_sandbox import Student
 
 if __name__ == '__main__':
-    pass
+    import ipdb; ipdb.set_trace()
