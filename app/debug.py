cf8c37cf96be130659cf9466a1141b5f4cf07d87
Ben Botsford <ben.botsford@flatironschool.com>
2022-09-20 07:09:57 -0500

    updated debug.py

--- app/debug.py
+++ app/debug.py
@@ -3,7 +3,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy_sandbox import Student
+from sqlalchemy_sandbox import Base, Student
 
 if __name__ == '__main__':
+
+    engine = create_engine('sqlite:///:memory:')
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
     import ipdb; ipdb.set_trace()
